ed9d806fe6194c8558d6452fecb332ad1c86d1c6
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2026-03-16 00:27:34 -0300

    test: Testing to inject dependencies on the controller.

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return {message: "API is running!"}', () => {
+      expect(appController.getHealthCheck()).toStrictEqual({"message":"API is running!"});
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHealthCheck(): {message: string} {
+    return {message: "API is running!"};
   }
 }
