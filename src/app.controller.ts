875942c595d559dda301b59c18699f24fc692d72
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2026-03-16 00:44:08 -0300

    feat: Enable versioning for the API.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
-@Controller()
+@Controller({
+  version: '1',
+})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHealthCheck(): {message: string} {
-    return this.appService.getHealthCheck();
+  getHealthCheck(): { message: string } {
+    return this.appService.getHealthCheck()
   }
 }
