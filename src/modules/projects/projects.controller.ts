6df00296d397926a406c045b610e51ef9b31f458
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2026-03-19 08:26:53 -0300

    feat: Create CRUD with mocked data.

--- src/modules/projects/projects.controller.ts
+++ src/modules/projects/projects.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ProjectsService } from './projects.service';
 
-@Controller('projects')
-export class ProjectsController {}
+@Controller({
+    version: '1',
+    path: 'projects',
+})
+export class ProjectsController {
+
+    constructor(
+        private readonly projectsService: ProjectsService
+    ) {}
+
+    @Get()
+    findAll() {
+        return this.projectsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.projectsService.findById(id);
+    }
+
+    @Post()
+    create(@Body() data: any) {
+        return this.projectsService.create(data);
+    }
+
+    @Put(':id')
+    update(
+        @Param('id') id: string, 
+        @Body() data: any,
+    ) {
+        return this.projectsService.update(id, data)
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id:string) {
+        return this.projectsService.remove(id)
+    }
+    
+}
--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+    findAll() {
+        return ['teste1', 'teste2']
+    }
+
+    findById(id: string) {
+        return 'teste1'
+    }
+
+    create(data: any) {
+        return 'create teste1'
+    }
+
+    update(id: string, data: any) {
+        return 'update teste1'
+    }
+
+    remove(id: string) {
+        return 'remove teste1'
+    }
+}
