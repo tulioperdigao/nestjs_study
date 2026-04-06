763b374d1557f5e90bdd3fe11aecb2565f103abe
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2026-04-05 22:27:27 -0300

    feat: Implement the task controller with CRUD methods and update the task module.

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
-@Controller('tasks')
-export class TasksController {}
+@Controller({
+    version: '1',
+    path: 'projects/:projectId/tasks'
+})
+export class TasksController {
+    constructor(private readonly tasksService: TasksService) {}
+
+    @Get()
+    findAllByProject(
+        @Param('projectId', ParseUUIDPipe) projectId: string
+    ){
+        return this.tasksService.findAllByProject(projectId);
+    }
+
+    @Post()
+    create(
+        @Param('projectId', ParseUUIDPipe) projectId: string,
+        @Body() data: any
+    ) {
+        return this.tasksService.create(projectId, data);
+    }
+
+    @Get(':taskId')
+    findOne(
+        @Param('projectId', ParseUUIDPipe) projectId: string,
+        @Param('taskId', ParseUUIDPipe) taskId: string,
+    ) {
+        return this.tasksService.findById(projectId, taskId);
+    }
+
+    @Put(':taskId')
+    update(
+        @Param('projectId', ParseUUIDPipe) projectId: string,
+        @Param('taskId', ParseUUIDPipe) taskId: string,
+        @Body() data: any
+    ) {
+        return this.tasksService.update(projectId, taskId, data);
+    }
+
+    @Delete(':taskId')
+    remove(
+        @Param('projectId', ParseUUIDPipe) projectId: string,
+        @Param('taskId', ParseUUIDPipe) taskId: string
+    ) {
+        return this.tasksService.delete(projectId, taskId);
+    }
+}
